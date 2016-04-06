0624ba1adcd6967f97cfc9ae9dd5884466d28c8e
Gergely Imreh <imrehg@gmail.com>
2016-04-06 21:17:00 +0800

    update favicon serving logic to use static folder properly

--- eatme_app.py
+++ eatme_app.py
@@ -2,9 +2,14 @@
 """
 EatMe - calories tracking app
 """
-from flask import Flask
+from flask import Flask, send_from_directory
 import os
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/x-icon')
 
 @app.route('/')
 def home():
