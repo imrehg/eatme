e8225afdee92527bf8b1ebd419c3846c2d30ff6c
Gergely Imreh <imrehg@gmail.com>
2016-04-11 21:02:47 +0800

    reorganize project layout

--- eatme/__init__.py
+++ eatme/__init__.py
@@ -14,11 +14,3 @@ def favicon():
 @app.route('/')
 def home():
     return "Hello!"
-
-if __name__ == '__main__':
-    # Run in stand-alone environment
-    try:
-        PORT = int(os.environ['PORT'])
-    except KeyError:
-        PORT = 5000
-    app.run(host='127.0.0.1', port=PORT, debug=True)
---
+++ runserver.py
@@ -0,0 +1,2 @@
+from eatme import app
+app.run(debug=True)
