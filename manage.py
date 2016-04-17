c5afbcd673ab5a99e62b5aa86efb98598c3d3057
Gergely Imreh <imrehg@gmail.com>
2016-04-17 18:54:09 +0800

    add password hash generation helper to the management script

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 """
 Management tools for EatMe
 """
-from flask.ext.script import Server, Shell, Manager
+from flask.ext.script import Server, Shell, Manager, prompt_pass
+from flask.ext.security.utils import encrypt_password
 
 # Import relevant parts of the app
 from eatme import app, db, models
@@ -16,6 +17,16 @@ def _make_context():
     return dict(app=app, db=db, models=models)
 
 manager.add_command("shell", Shell(make_context=_make_context))
+
+@manager.command
+def genpwhash():
+    """
+    Generate password hash for input
+    """
+    print("Current password hash algorithm: {}".format(app.config['SECURITY_PASSWORD_HASH']))
+    password = prompt_pass("Enter password to hash (hidden)")
+    print(encrypt_password(password))
+
 
 if __name__ == "__main__":
     manager.run()
