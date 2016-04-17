f9e8401bf35a08311e0b48f1b1900572d5335168
Gergely Imreh <imrehg@gmail.com>
2016-04-17 18:53:35 +0800

    update default config

--- config/default.py
+++ config/default.py
@@ -2,4 +2,15 @@ DEBUG = False
 DATABASE_URI = 'sqlite://'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-ADMIN_PASSWORD_HASH = 'pbkdf2:sha1:1000$RRo60efp$786b72f805d7558151ff2b24918d1713f5f5c617'
+## Security settings
+SECRET_KEY = 'i-have-not-changed-the-secret-key'
+SECURITY_PASSWORD_HASH = 'bcrypt'
+SECURITY_PASSWORD_SALT = 'i-have-not-changed-the-salt'
+SECURITY_TOKEN_AUTHENTICATION_KEY = 'auth_token'
+SECURITY_TRACKABLE = True
+# Note: http://mandarvaze.github.io/2015/01/token-auth-with-flask-security.html
+WTF_CSRF_ENABLED = False
+
+ADMIN_EMAIL = 'root@localhost'
+ADMIN_PASSWORD_HASH = '$2a$12$nTdjqiAwDXbCm3xtv2TkReaNtWXBLnqSDUu72v/JHgSkmfgSUw.pO'
+
